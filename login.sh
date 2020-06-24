e79494292ede24c2fcb0b0f99025a8af23ec03f2
David Bauer <mail@david-bauer.net>
2020-06-25 00:27:10 +0200

    update login.sh

--- login.sh
+++ login.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
-curl -c "/tmp/wifidarmstadt.cookie" -A "Mozilla/5.0 (X11; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0" -L "https://service.thecloud.eu/service-platform/macauthlogin/v1"
+
+
+curl -c "/tmp/wifidarmstadt.cookie" -A "Mozilla/5.0 (X11; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0" -L "https://service.thecloud.eu/service-platform/redirect/2783"
+curl -b "/tmp/wifidarmstadt.cookie" -c "/tmp/wifidarmstadt.cookie" -A "Mozilla/5.0 (X11; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0" -L "https://service.thecloud.eu/service-platform/macauthlogin/v1"
 curl -b "/tmp/wifidarmstadt.cookie" -d "terms=true" -A "Mozilla/5.0 (X11; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0" -L "https://service.thecloud.eu/service-platform/macauthlogin/v1/registration"
 rm /tmp/wifidarmstadt.cookie
